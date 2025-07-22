d61724a2e7d617fdd6890c47f9b82303958e34d6
FreeBird-spectre <sethabir699@gmail.com>
2025-07-22 19:32:32 +0530

    Update Basic_Bitwise_Operators 4A.cpp

--- Basic_Bitwise_Operators 4A.cpp
+++ Basic_Bitwise_Operators 4A.cpp
@@ -5,5 +5,34 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int a = 7; // 0111
-    int b = 8;
+    int a = 5; 
+    int b = 9;
+
+    int bitwise_AND = a & b;
+    int bitwise_OR = a | b;
+    int bitwise_XOR = a ^ b;
+    int bitwise_NOT = ~b;
+    int bitwise_LSHIFT = b << 2;
+    int bitwise_RSHIFT = a >> 1;
+
+    cout<<"AND : " << bitwise_AND <<endl;
+    cout<<"OR : " << bitwise_OR <<endl;
+    cout<<"XOR : " << bitwise_XOR <<endl;
+    cout<<"NOT : " << bitwise_NOT <<endl;
+    cout<<"L-shift : " << bitwise_LSHIFT << endl;
+    cout<<"R-shift : " << bitwise_RSHIFT << endl;
+
+    
+    cout <<"";
+    return 0;
+}
+
+AND : 1
+OR : 13
+XOR : 12
+NOT : -10
+L-shift : 36
+R-shift : 2
+
+
+=== Code Execution Successful ===
